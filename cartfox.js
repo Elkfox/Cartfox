9e1af108a76c1b5e9f6244fec60e72625a1c24b3
Oscar Strangio <oscar.strangio@me.com>
2018-07-31 16:19:21 +1000

    :tada: v3.0-alpha.1

--- cartfox.js
+++ cartfox.js
@@ -1,4 +1,6 @@
 const CartFox = function CartFox(configuration) {
+  'use strict';
+
   // Set up our Cartfox instance with the defined configuration
   const config = configuration;
   const cart = config.cart || {};
@@ -33,22 +35,26 @@ const CartFox = function CartFox(configuration) {
     dataType: 'json',
     method: 'POST',
   });
+
   // Merge and add the settings into the prototype
   this.cart = cart;
+
   // Keep track of items in an id centric structure
   this.items = this.createItems(cart);
   this.selectors = Object.assign(selectors, defaultSelectors);
   this.options = Object.assign(options, defaultOptions);
   this.renderCart = this.options.renderCart || this.renderCart;
-  this.onCartRender = this.options.onCartRender.bind(this) || null;
+  if (this.options.onRender && typeof this.options.onRender === 'function') { this.onRender = this.options.onRender.bind(this); }
 
   // Build the event listeners from the selectors
   this.buildEventListeners(this.selectors);
   jQuery(document).trigger('cartfox:ready', [this]);
   return this;
-};
+}
 
 CartFox.prototype.buildEventListeners = function buildEventListeners(selectors) {
+  'use strict';
+
   // Try and build event listeners using jQuery
   try {
     jQuery(document).on('click', selectors.addItem, this.addToCart.bind(this));
@@ -57,7 +63,7 @@ CartFox.prototype.buildEventListeners = function buildEventListeners(selectors) 
     jQuery(document).on('click', selectors.increaseQuantity, this.increaseQuantity.bind(this));
     jQuery(document).on('click', selectors.quickAdd, this.quickAdd.bind(this));
   } catch (error) {
-    console.warn(`Could not build event listeners: ${error}`);
+    console.warn('Could not build event listeners: ' + error);
   }
   return this;
 };
@@ -66,10 +72,12 @@ CartFox.prototype.buildEventListeners = function buildEventListeners(selectors) 
   Event based Functions
 */
 CartFox.prototype.addToCart = function onClickAddToCartButton(event) {
+  'use strict';
   event.preventDefault();
-  const id = jQuery('[name=id]').val();
-  const quantity = Number(jQuery('[name=quantity]').val());
+  const id = jQuery('[name="id"]').val();
+  const quantity = Number(jQuery('[name="quantity"]').val());
   const properties = {};
+
   if (jQuery('[name*=properties]').length > 0) {
     jQuery('[name*=properties]').each(function getProperty() {
       const key = jQuery(this).attr('name').split('[')[1].split(']')[0];
@@ -77,72 +85,87 @@ CartFox.prototype.addToCart = function onClickAddToCartButton(event) {
       properties[key] = value;
     });
   }
+
   const data = {
-    id,
-    quantity,
-    properties,
+    id: id,
+    quantity: quantity,
+    properties: properties,
   };
+
   this.addItem(data);
 };
 
 CartFox.prototype.quickAdd = function onClickQuickAddButton(event) {
+  'use strict';
   event.preventDefault();
+
   const id = Number(jQuery(event.currentTarget).data(this.getDataAttribute(this.selectors.quickAdd)));
   const quantity = Number(jQuery(event.currentTarget).data(this.getDataAttribute(this.selectors.quickAddQuantity))) || 1;
   const properties = jQuery(event.currentTarget).data(this.getDataAttribute(this.selectors.quickAddProperties)) || {};
-  console.log(properties);
   const data = {
-    id,
-    quantity,
-    properties,
+    id: id,
+    quantity: quantity,
+    properties: properties,
   };
+
   this.addItem(data);
 };
 
 CartFox.prototype.removeFromCart = function onClickRemoveButton(event) {
+  'use strict';
   event.preventDefault();
+
   const attribute = this.getDataAttribute(this.selectors.removeItem);
   const line = Number(jQuery(event.currentTarget).data(attribute));
+
   this.removeItemByLine(line);
 };
 
 CartFox.prototype.decreaseQuantity = function onClickDecreaseQuantityButton(event) {
+  'use strict';
   event.preventDefault();
+
   const attribute = this.getDataAttribute(this.selectors.decreaseQuantity);
   const line = Number(jQuery(event.currentTarget).data(attribute));
   const index = line - 1;
   const id = this.cart.items[index].id;
   const quantity = this.cart.items[index].quantity - 1;
+
   const success = function success(cart) {
     return jQuery(document).trigger('cartfox:itemQuantityDecreased', [cart]);
-  };
+  }
   const error = function error(error) {
     return jQuery(document).trigger('cartfox:cannotDecreaseItemQuantity', [error]);
-  };
+  }
   const options = {
-    success,
-    error,
-  };
+    success: success,
+    error: error,
+  }
+
   this.updateItemByLine(line, quantity, options);
 };
 
 CartFox.prototype.increaseQuantity = function onClickIncreaseQuantityButton(event) {
+  'use strict';
   event.preventDefault();
+
   const attribute = this.getDataAttribute(this.selectors.increaseQuantity);
   const line = Number(jQuery(event.currentTarget).data(attribute));
   const index = line - 1;
   const id = this.cart.items[index].id;
   const quantity = this.cart.items[index].quantity + 1;
+
   const success = function success(cart) {
     return jQuery(document).trigger('cartfox:itemQuantityIncreased', [cart]);
-  };
+  }
   const error = function error(error) {
     return jQuery(document).trigger('cartfox:cannotIncreaseItemQuantity', [error]);
-  };
+  }
   const options = {
-    success,
-    error,
-  };
+    success: success,
+    error: error,
+  }
+
   this.updateItemByLine(line, quantity, options);
 };
 /*
@@ -157,41 +180,47 @@ CartFox.prototype.increaseQuantity = function onClickIncreaseQuantityButton(even
 // Request a fresh cart from 'cart.js'
 // Then re-render the cart
 CartFox.prototype.getCart = function getCart(config) {
+  'use strict';
+
   const options = config || {};
   const success = options.success || function getCartSuccess(cart) {
-    this.beforeRender(cart);
+    this.updateCartObject(cart);
   }.bind(this);
 
   const request = {
     url: '/cart.js',
     method: 'GET',
-    success,
+    success: success,
   };
 
+  // Add the request to the queue
   this.queue.add(request);
   return this.cart;
-};
+}
 
-// Before rendering cart
+// Updating cart object
 // Update global this.cart from the returned 'update.js' or 'change.js' API callback
 // Then re-render the cart
-CartFox.prototype.beforeRender = function beforeRenderingCart(cart) {
-  jQuery(document).trigger('cartfox:beforeRender', [this.cart, cart]);
+CartFox.prototype.updateCartObject = function updateCartObjectFromNewCart(cart) {
+  'use strict';
+
   this.cart = cart;
   this.renderCart(this.cart);
-};
+}
 
 // Default cart rendering
-// Trigger a renderCart event and run any onCartRender functions
+// Trigger a renderCart event and run any onRender functions
 // Can be overwritten from new Cartfox init
 CartFox.prototype.renderCart = function renderCartFromObject() {
-  $(this.selectors.cartItemCount).html(this.cart.item_count);
-  jQuery(document).trigger('cartfox:renderCart', this.cart);
+  'use strict';
 
-  if (this.options.onCartRender && typeof this.options.onCartRender === 'function') {
-    this.options.onCartRender(this.cart);
+  $(this.selectors.cartItemCount).html(this.cart.item_count);
+  jQuery(document).trigger('cartfox:onRender', this.cart);
+
+  if (this.onRender && typeof this.onRender === 'function') {
+    this.onRender(this.cart);
   }
-};
+}
 
 // Create an AJAX request to add an item the cart
 // Accepts an object of the item to add:
@@ -199,6 +228,8 @@ CartFox.prototype.renderCart = function renderCartFromObject() {
 //  - quantity
 //  - properties
 CartFox.prototype.addItem = function addItem(data, config) {
+  'use strict';
+
   const item = data || {};
   const options = config || {};
 
@@ -214,7 +245,7 @@ CartFox.prototype.addItem = function addItem(data, config) {
   }.bind(this);
   options.error = options.error || function error(error) {
     return jQuery(document).trigger('cartfox:cannotAddToCart', [error]);
-  };
+  }.bind(this);
 
   const request = {
     url: '/cart/add.js',
@@ -224,6 +255,7 @@ CartFox.prototype.addItem = function addItem(data, config) {
   };
 
   // Add the request to the queue
+  jQuery(document).trigger('cartfox:beforeItemAdded');
   this.queue.add(request);
   return this;
 };
@@ -231,12 +263,14 @@ CartFox.prototype.addItem = function addItem(data, config) {
 // Remove item by id
 // Accepts an item id and config
 CartFox.prototype.removeItemById = function removeItemById(id, config) {
+  'use strict';
+
   const data = { updates: {} };
   const options = config || {};
   data.updates[id] = 0;
 
   options.success = options.success || function success(cart) {
-    this.beforeRender(cart);
+    this.updateCartObject(cart);
     return jQuery(document).trigger('cartfox:itemRemoved', [cart]);
   }.bind(this);
   options.error = options.error || function error(error) {
@@ -245,21 +279,25 @@ CartFox.prototype.removeItemById = function removeItemById(id, config) {
 
   const request = {
     url: '/cart/update.js',
-    data,
+    data: data,
     error: options.error,
     success: options.success,
-  };
+  }
 
+  // Add the request to the queue
+  jQuery(document).trigger('cartfox:beforeItemRemoved');
   this.queue.add(request);
   return this;
-};
+}
 
 // Remove item by line
 CartFox.prototype.removeItemByLine = function removeItemFromCartUsingLineindex(line, config) {
+  'use strict';
+
   const options = config || {};
 
   options.success = options.success || function success(cart) {
-    this.beforeRender(cart);
+    this.updateCartObject(cart)
     return jQuery(document).trigger('cartfox:itemRemoved', [cart]);
   }.bind(this);
   options.error = options.error || function error(error) {
@@ -269,25 +307,28 @@ CartFox.prototype.removeItemByLine = function removeItemFromCartUsingLineindex(l
   const request = {
     url: '/cart/change.js',
     data: {
-      line,
+      line: line,
       quantity: 0,
     },
     success: options.success,
     error: options.error,
   };
 
+  // Add the request to the queue
+  jQuery(document).trigger('cartfox:beforeItemRemoved');
   this.queue.add(request);
   return this;
-};
+}
 
 CartFox.prototype.updateItemById = function updateItemById(id, quantity, config) {
+  'use strict';
   const data = { updates: {} };
   const options = config || {};
 
   data.updates[id] = quantity;
 
   options.success = options.success || function success(cart) {
-    this.beforeRender(cart);
+    this.updateCartObject(cart)
     return jQuery(document).trigger('cartfox:itemUpdated', [cart]);
   }.bind(this);
   options.error = options.error || function error(error) {
@@ -296,20 +337,23 @@ CartFox.prototype.updateItemById = function updateItemById(id, quantity, config)
 
   const request = {
     url: '/cart/update.js',
-    data,
+    data: data,
     error: options.error,
     success: options.success,
-  };
+  }
 
+  // Add the request to the queue
+  jQuery(document).trigger('cartfox:beforeItemUpdated');
   this.queue.add(request);
   return this;
-};
+}
 
 CartFox.prototype.updateItemByLine = function updateItemFromCartUsingLineindex(line, quantity, config) {
+  'use strict';
   const options = config || {};
 
   options.success = options.success || function success(cart) {
-    this.beforeRender(cart);
+    this.updateCartObject(cart)
     return jQuery(document).trigger('cartfox:itemUpdated', [cart]);
   }.bind(this);
   options.error = options.error || function error(error) {
@@ -319,22 +363,25 @@ CartFox.prototype.updateItemByLine = function updateItemFromCartUsingLineindex(l
   const request = {
     url: '/cart/change.js',
     data: {
-      line,
-      quantity,
+      line: line,
+      quantity: quantity,
     },
     success: options.success,
     error: options.error,
   };
 
+  // Add the request to the queue
+  jQuery(document).trigger('cartfox:beforeItemUpdated');
   this.queue.add(request);
   return this;
-};
+}
 
 CartFox.prototype.clearCart = function clearCart(config) {
+  'use strict';
   const options = config || {};
 
   options.success = options.success || function success(cart) {
-    this.beforeRender(cart);
+    this.updateCartObject(cart);
     return jQuery(document).trigger('cartfox:cartCleared', [cart]);
   }.bind(this);
   options.error = options.error || function error(error) {
@@ -347,9 +394,11 @@ CartFox.prototype.clearCart = function clearCart(config) {
     error: options.error,
   };
 
+  // Add the request to the queue
+  jQuery(document).trigger('cartfox:beforeCartCleared');
   this.queue.add(request);
   return this;
-};
+}
 
 /*
   END AJAX API Functions
@@ -365,12 +414,14 @@ CartFox.prototype.getDataAttribute = function getAttributeFromDataAttributeSelec
 
 CartFox.prototype.createItems = function createAnIdCentricDataStuctureFromTheCart(cart) {
   const items = {};
+
   for (let i = 0; i < cart.items.length; i++) {
     items[cart.items[i].id] = {
-      line: i + 1,
-      quantity: cart.items[i].quantity,
-    };
+      line: i+1,
+      quantity: cart.items[i].quantity
+    }
   }
+
   return items;
 };
 
